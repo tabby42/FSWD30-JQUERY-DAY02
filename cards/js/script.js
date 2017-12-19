d328359d2b2337cc7ae3a2c987258ea44bf360ea
tabby42 <irene.theiss@gmx.at>
2017-12-19 14:51:59 +0100

    add functions for ativate/deactivate drag

--- cards/js/script.js
+++ cards/js/script.js
@@ -1,29 +1,50 @@
-$(document).ready(function() {
-	var numbers = [1,2,3,4,5,6,7,8,9,10];
+jQuery(document).ready(function ($) {
+	'use strict';
+	var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	var dragAttempts = 0;
 
-	$( ".draggable" ).draggable({
+	$( ".draggable" ).draggable ({
 		// when not dropped, the item will revert back to its initial position
 		revert: "invalid", 
-		containment: "document"
+		containment: "document",
 	});
-	$( ".droppable" ).droppable({
-		accept: function( d ) { 
-	        if( $(this).text()  === d.text() ){ 
+
+	$( ".droppable" ).droppable ({
+		//accept only, if numbers match
+		accept: function ( d ) { 
+	        if( $(this).attr("data-num") === d.text() ){ 
 	            return true;
 	        }
     	},
-		drop: function( event, ui ) {
-        	$( this ).addClass( "ui-state-highlight" );
+		drop: function ( event, ui ) {
             $( ui.draggable[0] ).addClass( "dropped-color" );
-      }
+            $( ui.draggable ).css( {background: randomColor()} );
+            //snap to same position 
+            $(ui.draggable).detach().css({top: 0,left: 0}).appendTo(this);
+      	},
+      	activate: function( event, ui ) {
+      		$( ui.draggable ).addClass( "ui-state-highlight" );
+      		dragAttempts ++;
+      		if (dragAttempts > 50) {
+      			$("#score").text( "Game over!" );
+      			$( ".droppable" ).droppable( "option", "disabled", true );
+      			$( ".draggable" ).draggable( 'disable' );
+      		} else {
+      			$("#score").text( "You dragged " + dragAttempts + " times." );
+      		}
+      	},
+      	deactivate: function( event, ui ) {
+      		$( ui.draggable ).removeClass( "ui-state-highlight" );
+      	}
 	});
 
-	function shuffle(array) {
+	//shuffle numbers randomly
+	function shuffle ( array ) {
 		var currentIndex = array.length, 
 			temporaryValue, 
 			randomIndex;
 		// While there remain elements to shuffle...
-		while (0 !== currentIndex) {
+		while ( 0 !== currentIndex ) {
 		    // Pick a remaining element...
 		    randomIndex = Math.floor(Math.random() * currentIndex);
 		    currentIndex -= 1;
@@ -35,5 +56,22 @@ $(document).ready(function() {
 	    return array;
 	}
 
+	//random Order for cards
 	var randomOrder = shuffle(numbers);
+	$.each($(".card.draggable"), function( index, value ) {
+		$(this).text(randomOrder[index]);
+	});
+	var secondRandomOrder = shuffle(numbers);
+	$.each($(".card.droppable"), function( index, value ) {
+		$(this).attr("data-num", randomOrder[index]);
+	});
+
+	function randomColor () {
+		var red = Math.floor(Math.random() * 256);
+		var blue = Math.floor(Math.random() * 256);
+		var green = Math.floor(Math.random() * 256);
+		return "rgb(" + red + ","  + green + ","  + blue + ")";
+	}
 });
+
+
